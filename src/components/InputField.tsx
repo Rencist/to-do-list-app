d46902ec7cb8a346500fd26a4c584f8ddabcd94f
Rencist <dafianakhdan@gmail.com>
2022-09-02 17:27:01 +0700

    feat: add delete and done feature

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './style.css';
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const InputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-      <input type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a task" className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        InputRef.current?.blur();
+      }}
+    >
+      <input ref={InputRef} type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a task" className="input__box" />
       <button className="input_submit" type="submit">
         Go
       </button>
